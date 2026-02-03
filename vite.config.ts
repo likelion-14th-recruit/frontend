6fa905008d86693f04c9612792eedd33cdafb964
hyunneee <sh_oh@u.sogang.ac.kr>
2026-02-04 03:02:37 +0900

    FEAT:HOME UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://likelion14-recruit-staging.duckdns.org", // test 주소
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // /api 부분을 지우고 전달
+      },
+    },
+  },
+});
