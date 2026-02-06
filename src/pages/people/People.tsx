da73ff100d28b31523d38bf648e6f7d4cba5ef73
이예나 <iyena@iyenas-MacBook-Pro.local>
2026-02-06 22:52:54 +0900

    loading-scenario

--- src/pages/people/People.tsx
+++ src/pages/people/People.tsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PageTitle from "../../components/people+project/PageTitle";
 import PeopleGrid from "../../components/people+project/PeopleGrid";
 import { type PeopleType } from "../../types/people";
 
 type TabValue = "all" | "backend" | "frontend" | "product_design";
+
+const TAB_TO_PART_MAP: Record<
+  Exclude<TabValue, "all">,
+  PeopleType["part"]
+> = {
+  backend: "BACKEND",
+  frontend: "FRONTEND",
+  product_design: "PRODUCT_DESIGN",
+};
+
 
 const People = () => {
 
@@ -11,100 +21,108 @@ const People = () => {
   
   //mock-up data
   const peopleData: PeopleType[] = [
-  {
-    cohort: 14,
-    imageUrl: "http://dummy1",
-    name: "김지오",
-    part: "BACKEND",
-    position: "PRESIDENT",
-  },
-  {
-    cohort: 14,
-    imageUrl: "http://dummy2",
-    name: "이예나",
-    part: "FRONTEND",
-    position: "VICE_PRESIDENT",
-  },
-  {
-    cohort: 14,
-    imageUrl: "http://dummy3",
-    name: "전해찬",
-    part: "BACKEND",
-    position: "VICE_PRESIDENT",
-  },
-  {
-    cohort: 14,
-    imageUrl: "http://dummy7",
-    name: "문금미",
-    part: "FRONTEND",
-    position: "PART_LEADER",
-  },
-  {
-    cohort: 14,
-    imageUrl: "http://dummy4",
-    name: "송명은",
-    part: "PRODUCT_DESIGN",
-    position: "PART_LEADER",
-  },
-  {
-    cohort: 14,
-    imageUrl: "http://dummy10",
-    name: "최성민",
-    part: "BACKEND",
-    position: "PART_LEADER",
-  },
-  {
-    cohort: 14,
-    imageUrl: "http://dummy5",
-    name: "오서현",
-    part: "PRODUCT_DESIGN",
-    position: "MEMBER",
-  },
-  {
-    cohort: 14,
-    imageUrl: "http://dummy11",
-    name: "유지오",
-    part: "BACKEND",
-    position: "MEMBER",
-  },
-  {
-    cohort: 14,
-    imageUrl: "http://dummy12",
-    name: "전수아",
-    part: "BACKEND",
-    position: "MEMBER",
-  },
-  {
-    cohort: 14,
-    imageUrl: "http://dummy8",
-    name: "주현수",
-    part: "FRONTEND",
-    position: "MEMBER",
-  },
-  {
-    cohort: 14,
-    imageUrl: "http://dummy6",
-    name: "천사은",
-    part: "PRODUCT_DESIGN",
-    position: "MEMBER",
-  },
-  {
-    cohort: 14,
-    imageUrl: "http://dummy9",
-    name: "한서정",
-    part: "FRONTEND",
-    position: "MEMBER",
-  },
-];
+    {
+      cohort: 14,
+      imageUrl: "http://dummy1",
+      name: "김지오",
+      part: "BACKEND",
+      position: "PRESIDENT",
+    },
+    {
+      cohort: 14,
+      imageUrl: "http://dummy2",
+      name: "이예나",
+      part: "FRONTEND",
+      position: "VICE_PRESIDENT",
+    },
+    {
+      cohort: 14,
+      imageUrl: "http://dummy3",
+      name: "전해찬",
+      part: "BACKEND",
+      position: "VICE_PRESIDENT",
+    },
+    {
+      cohort: 14,
+      imageUrl: "http://dummy7",
+      name: "문금미",
+      part: "FRONTEND",
+      position: "PART_LEADER",
+    },
+    {
+      cohort: 14,
+      imageUrl: "http://dummy4",
+      name: "송명은",
+      part: "PRODUCT_DESIGN",
+      position: "PART_LEADER",
+    },
+    {
+      cohort: 14,
+      imageUrl: "http://dummy10",
+      name: "최성민",
+      part: "BACKEND",
+      position: "PART_LEADER",
+    },
+    {
+      cohort: 14,
+      imageUrl: "http://dummy5",
+      name: "오서현",
+      part: "PRODUCT_DESIGN",
+      position: "MEMBER",
+    },
+    {
+      cohort: 14,
+      imageUrl: "http://dummy11",
+      name: "유지오",
+      part: "BACKEND",
+      position: "MEMBER",
+    },
+    {
+      cohort: 14,
+      imageUrl: "http://dummy12",
+      name: "전수아",
+      part: "BACKEND",
+      position: "MEMBER",
+    },
+    {
+      cohort: 14,
+      imageUrl: "http://dummy8",
+      name: "주현수",
+      part: "FRONTEND",
+      position: "MEMBER",
+    },
+    {
+      cohort: 14,
+      imageUrl: "http://dummy6",
+      name: "천사은",
+      part: "PRODUCT_DESIGN",
+      position: "MEMBER",
+    },
+    {
+      cohort: 14,
+      imageUrl: "http://dummy9",
+      name: "한서정",
+      part: "FRONTEND",
+      position: "MEMBER",
+    },
+  ];
 
   //필터
   const filteredPeople =
-    activeTab === "all"
-      ? peopleData
-      : peopleData.filter(
-          (person) =>
-            person.part === activeTab.toUpperCase()
-        );
+  activeTab === "all"
+    ? peopleData
+    : peopleData.filter(
+        (person) => person.part === TAB_TO_PART_MAP[activeTab]
+      );
+
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+  }, []);
 
 
   return (
@@ -125,7 +143,7 @@ const People = () => {
   }}
     />
 
-    <PeopleGrid people={filteredPeople} />
+    <PeopleGrid people={filteredPeople} isLoading={isLoading} />
     </>
   );
 };
